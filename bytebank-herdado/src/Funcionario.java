f5c5931958eb30e977b477f37e766b52dfa6d5ef
Andreia <andreiao.sales@hotmail.com>
2022-06-30 16:51:56 -0300

    Polimorfismo 1 concluido

--- bytebank-herdado/src/Funcionario.java
+++ bytebank-herdado/src/Funcionario.java
@@ -3,11 +3,26 @@ public class Funcionario {
 	private String nome;
 	private String cpf;
 	private double salario;
+	private int tipo = 0;
 	
 	public double getbonificacao() {
-	 return this.salario * 0.1;
-	}
-	
+
+        if(this.tipo == 0) { // Funcionário comum;
+            return this.salario * 0.1;
+        } else if(this.tipo == 1) { // Gerente;
+            return this.salario;
+        } else {
+            return this.salario + 1000.0;
+        }
+    }
+
+    public void setTipo(int tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getTipo() {
+        return tipo;
+    }
 	public String getNome() {
 		return nome;
 	}
