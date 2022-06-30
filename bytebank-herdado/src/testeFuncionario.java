354daf5e4df45725c180ca3c3ba672bb6e5460ab
Andreia <andreiao.sales@hotmail.com>
2022-06-30 16:22:50 -0300

    polimorfismo

---
+++ bytebank-herdado/src/testeFuncionario.java
@@ -0,0 +1,17 @@
+
+public class testeFuncionario {
+	
+	public static void main(String[] args) {
+		
+		Funcionario nico = new Funcionario ();
+		nico.setNome("Nico Steppan");
+		nico.setCpf("222222222-22");
+		nico.setSalario(2590.80);
+		
+		System.out.println(nico.getbonificacao());
+		System.out.println(nico.getNome());
+				
+		
+	}
+
+}
