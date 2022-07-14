012509444b7e162a132f165d3ab3aca0df74652c
Andreia <andreiao.sales@hotmail.com>
2022-07-13 22:52:54 -0300

    Consultas com JPQL

--- 2121-jpa-hibernate-persistencia-aula_4/src/main/java/br/com/alura/loja/modelo/Produto.java
+++ 2121-jpa-hibernate-persistencia-aula_4/src/main/java/br/com/alura/loja/modelo/Produto.java
@@ -4,8 +4,6 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 
 import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -26,11 +24,9 @@ public class Produto {
 	
 	@ManyToOne
 	private Categoria categoria;
-
+	
 	public Produto() {
 	}
-	
-	
 	
 	public Produto(String nome, String descricao, BigDecimal preco, Categoria categoria) {
 		this.nome = nome;
@@ -38,24 +34,6 @@ public class Produto {
 		this.preco = preco;
 		this.categoria = categoria;
 	}
-
-	public LocalDate getDataCadastro() {
-		return dataCadastro;
-	}
-
-	public void setDataCadastro(LocalDate dataCadastro) {
-		this.dataCadastro = dataCadastro;
-	}
-
-	public Categoria getCategoria() {
-		return categoria;
-	}
-
-	public void setCategoria(Categoria categoria) {
-		this.categoria = categoria;
-	}
-
-	
 
 	public Long getId() {
 		return id;
@@ -87,6 +65,22 @@ public class Produto {
 
 	public void setPreco(BigDecimal preco) {
 		this.preco = preco;
+	}
+
+	public LocalDate getDataCadastro() {
+		return dataCadastro;
+	}
+
+	public void setDataCadastro(LocalDate dataCadastro) {
+		this.dataCadastro = dataCadastro;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
 	}
 
 }
