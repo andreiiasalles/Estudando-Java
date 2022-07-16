afa2822cd15656a07d5c2dcf726fdc7ce15a1d26
Andreia <andreiao.sales@hotmail.com>
2022-07-16 11:50:21 -0300

    Concluido java collections

--- gerenciador-de-cursos/src/br/com/alura/Curso.java
+++ gerenciador-de-cursos/src/br/com/alura/Curso.java
@@ -2,15 +2,24 @@ package br.com.alura;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.Set;
 
 public class Curso {
 
 	private String nome;
 	private String instrutor;
 	private List<Aula> aulas = new LinkedList<Aula>();
-
+	private Set<Aluno> alunos = new LinkedHashSet<>();
+	private Map<Integer, Aluno>	matriculaParaAluno = new HashMap<>();
+	
 	public Curso(String nome, String instrutor) {
 		this.nome = nome;
 		this.instrutor = instrutor;
@@ -42,4 +51,26 @@ public class Curso {
 	public String toString() {
 		return "[Curso: " + nome + ", tempo total: " + this.getTempoTotal() + "," + "aulas: " + this.aulas + "]";
 	}
-}
+
+	public void matricula(Aluno aluno) {
+		this.alunos.add(aluno);
+		this.matriculaParaAluno.put(aluno.getNumeroMatricula(), aluno);
+	}
+	
+	public Set<Aluno> getAlunos() {
+		return Collections.unmodifiableSet(alunos);
+	}
+
+	public boolean estaMatriculado(Aluno aluno) {
+		return this.alunos.contains(aluno);
+	}
+
+	public Aluno buscaMatriculado(int numero) {
+		if(!matriculaParaAluno.containsKey(numero))
+			 throw new NoSuchElementException("Matricula " + numero
+			            + " não encontrada");
+	    return matriculaParaAluno.get(numero);
+	    }
+	
+	   }
+
